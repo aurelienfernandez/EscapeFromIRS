81178465365df4ce325f7591296ce0635c27434b
Aurelien FERNANDEZ <aurelien.fernandez@algosup.com>
2024-02-09 16:01:50 +0100

    Removed the add, sadge

--- IRS.js
+++ IRS.js
@@ -1,4 +1,4 @@
-
+import "./IRS.json"
 // Function to find a chapter by its id
 function findChapterById(chapters, id) {
     return chapters.find(chapter => chapter.id === id);
@@ -7,32 +7,25 @@ function findChapterById(chapters, id) {
 
 
 function Change(number) {
+
+    $.getJSON("IRS.json", function (data) {
+        const chapter = findChapterById(data.chapters, number);
+        TitleDiv = document.getElementById("title");
+        chapterDiv = document.getElementById("chapter");
+        choiceDiv = document.getElementById("choices");
+        choiceDiv.innerHTML = "";
+
+        TitleDiv.innerHTML = chapter.title;
+        chapterDiv.innerHTML = chapter.content;
+
+        chapter.choices.forEach(choice => {
+            if (choice.destination != 0) {
+                choiceDiv.innerHTML += "<div>" + choice.text + " <a href='javascript:void(0)' onclick='Change(" + choice.destination + ")'>" + choice.destination + "</a></div>"
+            } else {
+                choiceDiv.innerHTML = " <a href='javascript:void(0)' onclick='Change(" + choice.destination + ")'>" + choice.text + "</a>"
+            }
+        });
+    })
     // Fetch the JSON file
-    fetch('./IRS.json',{method: 'GET'})
-        .then(response => response.json())
-        .then(data => {
-            // Accessing the chapter with id "2"
-            const chapter = findChapterById(data.chapters, number);
-            TitleDiv = document.getElementById("title");
-            chapterDiv = document.getElementById("chapter");
-            choiceDiv = document.getElementById("choices");
-            choiceDiv.innerHTML = "";
 
-            TitleDiv.innerHTML = chapter.title;
-            chapterDiv.innerHTML = chapter.content;
-
-            chapter.choices.forEach(choice => {
-                if (choice.destination != 0) {
-                    choiceDiv.innerHTML += "<div>" + choice.text + " <a href='javascript:void(0)' onclick='Change(" + choice.destination + ")'>" + choice.destination + "</a></div>"
-                }else{
-                    choiceDiv.innerHTML = " <a href='javascript:void(0)' onclick='Change(" + choice.destination + ")'>" + choice.text + "</a>"
-                }
-            });
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-
-}
-
-function CloseAD(){
-    document.getElementById("ad").style.display="none";
 }
